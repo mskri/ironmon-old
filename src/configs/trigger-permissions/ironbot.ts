389b7e81fa835e5fd12d28abd60040f015949b7b
Sakari Mursu <sakari@mursu.dev>
2019-08-06 20:19:15 +0300

    Fix getRoleId trigger

--- src/configs/trigger-permissions/ironbot.ts
+++ src/configs/trigger-permissions/ironbot.ts
@@ -2,7 +2,7 @@ import { TriggerPermissions } from '../../typings';
 
 const TriggerPermissions: TriggerPermissions[] = [
     {
-        triggers: ['sayHello', 'getChannelId', 'getEmojiId'],
+        triggers: ['sayHello', 'getChannelId', 'getEmojiId', 'getRoleId'],
         guildId: '369588869794103297', // ironbot
         channels: {
             blacklisted: [],
--- src/message-triggers/generic/get-role-id.ts
+++ src/message-triggers/generic/get-role-id.ts
@@ -6,10 +6,8 @@ export default createMessageTrigger({
     name: 'getRoleId',
     trigger: new RegExp(/^!role-id\b/),
     execute: (message: Message) => {
-        sendToChannel(message.channel, this.message.channel.id);
-
-        const roleNameToSearchFor = this.message.content.replace(this.trigger, '').trim();
-        const channel = <TextChannel>this.message.channel;
+        const roleNameToSearchFor = message.content.replace(this.default.trigger, '').trim();
+        const channel = <TextChannel>message.channel;
         const role = channel.guild.roles.find(role => role.name.toLowerCase() === roleNameToSearchFor.toLowerCase());
 
         if (role) {
