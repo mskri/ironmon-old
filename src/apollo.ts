8718110f3f17ae82fd028acc754a56fd5386f178
Sakari Mursu <sakari@mursu.dev>
2019-08-10 21:04:36 +0300

    Add apollo for graphql

---
+++ src/apollo.ts
@@ -0,0 +1,11 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import fetch from 'node-fetch';
+import { createHttpLink } from 'apollo-link-http';
+
+const apolloClient = new ApolloClient({
+    link: createHttpLink({ uri: 'http://localhost:5000/graphql', fetch: fetch }),
+    cache: new InMemoryCache()
+});
+
+export default apolloClient;
