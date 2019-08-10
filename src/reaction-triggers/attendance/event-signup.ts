8a6689ddb260952c63cdc0277c3a16f2051854d6
Sakari Mursu <sakari@mursu.dev>
2019-08-10 18:32:07 +0300

    Change event signup reactions to their names instead of IDs

--- src/reaction-triggers/attendance/event-signup.ts
+++ src/reaction-triggers/attendance/event-signup.ts
@@ -5,9 +5,8 @@ import { createReactionTrigger } from '../../triggers/factory';
 export default createReactionTrigger({
     name: 'eventSignupReaction',
     reactions: [
-        '481485635836837888', // accepted
-        '481485663376637952', // tentative
-        '481485649732698124' // declined
+        'accepted', // :accepted:
+        'declined' // :declined:
     ],
     onAddReaction: (message: Message, event: ReactionEvent) => {
         event.reaction.remove();
